67acee11c9760e2422c48e8b34bd727477cfab6f
2019-SOW-DEV-004 <2019sowdev004@gmail.com>
2019-05-09 23:57:49 +0200

    Implemented UI test for test_ShouldReturnFifteenAll_WhenPlayersScoreOnePointEach

--- TennisUITests/ViewController/TennisViewControllerTests.swift
+++ TennisUITests/ViewController/TennisViewControllerTests.swift
@@ -5,4 +5,14 @@ class TennisViewControllerTests: XCTestCase {
     override func setUp() {
         XCUIApplication().launch()
     }
+    
+    func test_ShouldReturnFifteenAll_WhenPlayersScoreOnePointEach() {
+        let app = XCUIApplication()
+        app.buttons["player1Scores"].tap()
+        app.buttons["player2Scores"].tap()
+        
+        let score = app.staticTexts["15 - 15"]
+        
+        XCTAssertTrue(score.exists)
+    }
 }
