677afcc3cd47d20c77f3557bc7bd29f54fe2d18f
2019-SOW-DEV-004 <2019sowdev004@gmail.com>
2019-05-09 23:42:49 +0200

    Handled IBAction for firstPlayer and secondPlayer

--- Tennis/ViewController/TennisViewController.swift
+++ Tennis/ViewController/TennisViewController.swift
@@ -1,8 +1,31 @@
 import UIKit
 
-class TennisViewController: UIViewController {
-
+class TennisViewController: UIViewController, TennisView {
+    private var tennisGamePresenter: TennisPresenter!
+    @IBOutlet weak var gameStatus: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tennisGamePresenter = buildTennisGamePresenter()
+    }
+    
+    @IBAction func playerOneScores(_ sender: Any) {
+        tennisGamePresenter.scoresPoint(.FirstPlayer)
+    }
+    
+    @IBAction func playerTwoScores(_ sender: Any) {
+        tennisGamePresenter.scoresPoint(.SecondPlayer)
+    }
+    
+    //Mark: Helper methods
+    private func buildTennisGamePresenter() -> TennisPresenter? {
+        let firstPlayer = Player("FirstPlayer")
+        let secondPlayer = Player("SecondPlayer")
+        
+        return TennisPresenter.init(firstPlayer, secondPlayer, self)
+    }
+    
+    func displayScore(_ playersGameScore: String) {
+        self.gameStatus.text = playersGameScore
     }
 }
