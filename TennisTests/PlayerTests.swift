ab0aa9d6ba1a5676b17fdf8da5f5fa130c410001
2019-SOW-DEV-004 <2019sowdev004@gmail.com>
2019-05-09 19:07:50 +0200

    Extracted player initialisation in test setup method

--- TennisTests/PlayerTests.swift
+++ TennisTests/PlayerTests.swift
@@ -2,24 +2,25 @@ import XCTest
 @testable import Tennis
 
 class PlayerTests: XCTestCase {
+    
+    private var player: Player!
+    
+    override func setUp() {
+        player = Player.init("PlayerOne")
+    }
 
     func test_ShouldReturnPlayerName_WhenPlayerIsInitialized() {
-        let player = Player.init("PlayerOne")
         
         XCTAssertEqual("PlayerOne", player.name())
     }
     
     func test_ShouldUpdateScore_WhenPlayerWinsAPoint() {
-        let player = Player.init("PlayerOne")
-        
         player.updateScore()
         
         XCTAssertEqual(1, player.currentScore())
     }
     
     func test_ShouldReturnTranslateScoreWithTennisPointRepresentation_WhenPlayerWinsAPoint() {
-        let player = Player.init("PlayerOne")
-        
         player.updateScore()
         
         XCTAssertEqual("15", player.translateScore())
